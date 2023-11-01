03c29f638c9590b6ac12f00fc50f7d16f60201c7
Travis Fowlston <travisfowlston@ymail.com>
2023-10-31 20:12:52 -0700

    Still working on JS file

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
-var alphaAPIKey = "O1GHS9U2N3JN93J1"
+var loganAlphaKey = "O1GHS9U2N3JN93J1"
 var marketAuxAPIKey = "0q9h2RqrW3R4xzDRc1WADtChaNPpwLUPxY2z6bJf"
-var otherAlphaKey = "IT2CROV94Q2OSDII"
+var travisAlphaKey = "IT2CROV94Q2OSDII"
 
 var newsURL = "https://api.marketaux.com/v1/news/all?symbols=TSLA,AMZN,MSFT&filter_entities=true&language=en&api_token=YOUR_API_TOKEN"
-var stocksURL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${userInput}&apikey=${alphaAPIKey}`
 
 var submitButton = document.getElementById("submit-btn");
 
@@ -13,7 +12,7 @@ submitButton.addEventListener("click", function () {
    
     /* Uses the user input to generate the url path by inserting the company name or symbol. */
     var searchByCompany = document.getElementById("userInput").value;
-    var stocksURL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchByCompany}&apikey=${otherAlphaKey}`
+    var stocksURL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchByCompany}&apikey=${travisAlphaKey}`
     
     /* Fetches and parses the response using json. */
     fetch(stocksURL)
@@ -31,12 +30,44 @@ submitButton.addEventListener("click", function () {
 
     for (var i = 0; i < bestMatch.length; i++) {
       var symbolResponse = bestMatch[i];
-      console.log(symbolResponse.name); // Log the name property to check if it's defined
+      console.log(symbolResponse['1. symbol'] + ", " + symbolResponse['2. name']); // Log the name property to check if it's defined
 
       let buttonEl = document.createElement('button');
       buttonEl.classList.add("btn-small");
-      buttonEl.innerHTML = symbolResponse.name;
+      buttonEl.innerHTML = symbolResponse['1. symbol'] + ", " + symbolResponse['2. name'];
       btnCard.append(buttonEl);
     }
   })
+  // Step 1: Create the event handler function
+function handleClick(event) {
+  // Perform the desired action
+  var symbol = event.target.textContent.split(",")[0].trim();
+  var getDailyStock = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${travisAlphaKey}`
+  
+  fetch(getDailyStock)
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Error: " + response.status);
+    }
+  })
+  .then(function (data) {
+    console.log(data);
+   let openPrice = data.times_series_daily
+   console.log(openPrice)
+})
+}
+
+// Step 2: Target the parent element
+var parentElement = document.getElementById("tickers");
+
+// Step 3: Attach the event listener to the parent element
+parentElement.addEventListener("click", function(event) {
+  // Step 4: Check if the clicked element matches the dynamically generated buttons
+  if (event.target.matches("button")) {
+    // Step 5: Perform the desired action
+    handleClick(event);
+  }
+});
 })
